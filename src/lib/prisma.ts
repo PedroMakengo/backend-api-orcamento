210b0a87918d3680806acab75857c15c0a19cea8
PedroMakengo <pedromakengomiguel@gmail.com>
2025-11-28 13:51:38 +0100

    Resolvido o erro

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-// import { PrismaClient } from "../generated/client/client";
 import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
